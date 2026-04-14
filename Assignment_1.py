a53d5dcf49840ec31503b8ec28cc02f1aba92630
Madlene Achieng <achienglina@gmail.com>
2026-04-15 00:10:35 +0300

    Updated Assignment_1

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,9 +1,23 @@
-first = input("First: ")
-first = int(10)
-second = input("Second: ")
-second = int(20)
-Sum = first + second
-print(Sum)
+print("\tQuestion1")
+# This is muilti-line comment
+# 1. Create program acting as simple calculator
+# 2. Take tWo numbers and an operator as input
+x = input(3 + 2) # assign variable x
+# 3. Perform the corresponding arithmetic
+print(x) # display the answer
 
+
+print("\n\tQuestion2")
+ # Create program that generates and prints random no
+import random #import random library
+y = input(random.random) # value to variable
+print(random.randint(1,100)) # use to show specific 
+
+
+print("\nQuestion3")
+# Create program that calculates and prints square root
+import math # import library to activate function
+p = input(math.sqrt(144)) # make value a variable
+print(p)
 
 
