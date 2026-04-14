9e24dabe87893c38b5103bada1e8d4d700e59584
Madlene Achieng <achienglina@gmail.com>
2026-04-15 00:16:27 +0300

    Practice 3

---
+++ hello_practice3.py
@@ -0,0 +1,8 @@
+# input() gets info from users
+# to retain value it is assigned a variable e.g name
+# print then gives back the value (displays the info)
+name = input("Enter Your name:")
+country = "Kenya"
+print(name, "comes from", country)
+# note that the terminal is now interactive
+# Input() then assign variable then add print()
