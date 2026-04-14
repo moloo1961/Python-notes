3fe4a33943a1efb29e05355b4b3ff9d3874ec5cc
Madlene Achieng <achienglina@gmail.com>
2026-04-15 00:16:03 +0300

    Practice 2

---
+++ hello_practice2.py
@@ -0,0 +1,22 @@
+print("Marlene1\tMarlene2") # to use \t to create tab
+print("Your Learning Path:\n\t-Python Basics\n\t-Data Engineering\n\t-AI")
+# Use triple quotes to write in multiple line strings
+# \n not needed since its job is to add line
+print("""Your learning Path:
+      \t-Python Basics 
+      \t-Data Engineering
+      \t-AI""")
+# How to create variables
+# The variables are preceded by space then comma
+# Variables are followed by comma if variable comes after
+# Values are encased in parentheses
+name = "Marlene" # stored variable
+language ="python" #stored variable
+print("My name is", name)
+print(name, "is learning", language)
+print(name, "wants to become a", language, "expert")
+# Variables make programs dynamic
+name = "Marlene"
+print("info@datawith", name, ".com")
+print("support@datawith", name, ".com")
+print("www.datawith", name, ".com")
