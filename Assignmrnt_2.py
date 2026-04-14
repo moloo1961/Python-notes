3788f32437b69ab5bab312b7bdf591ea27d26bdb
Madlene Achieng <achienglina@gmail.com>
2026-04-15 00:15:20 +0300

    Assignment 2

---
+++ Assignmrnt_2.py
@@ -0,0 +1,14 @@
+# Program using input to prompt user
+name = input("Enter your name:") # make input a variable
+Greeting = "Hello" # make Hello a greeting variable
+print(Greeting, name) # display result
+
+# Program to promt user for hours and rate 
+# to compute gross pay
+hours = input("Enter hours:")
+rate = input("Enter rate:")
+gross_pay = "hours * rate"
+print("gross_pay")
+
+
+
---
+++ hello_practice.py
@@ -0,0 +1,20 @@
+print("Hi, this is my first practice python code")
+print("Hi python") # learning to use print
+print('Hello python') # learning double/single quote
+print("----------------------") # creating cool title
+print("  practice and learn   ")
+print("----------------------")
+print("            ") # creating a dinasour 
+print("          ___")
+print("         / _)")
+print("  .-^^^-/ /  ")
+print("__/       /  ")
+print("__.|_|-|_|")
+print("Hi \"python\"") # escape double quote
+print('Hi \"python\"')
+print('Hi "python"') # double and single parentheses
+# how to use real backslash in a code - double when writing
+print("Path: C: \\Users\\Marlene")
+print("Marlene1\n") # use \n to separate code
+print("Marlene2")
+print("Marlene1\nMarlene2")
